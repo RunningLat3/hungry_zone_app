9c0f06e18e383978782aa6ababcdca1114c60393
Frank Claveria <frankrobertclaveria@accenture.com>
2022-10-23 23:12:48 +0800

    Add Tags and Food Page

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,13 +7,17 @@ import { HeaderComponent } from './components/partials/header/header.component';
 import { HomeComponent } from './components/pages/home/home.component';
 import { RatingModule } from 'ng-starrating';
 import { SearchBarComponent } from './components/partials/search-bar/search-bar.component';
+import { TagsComponent } from './components/partials/tags/tags.component';
+import { FoodPageComponent } from './components/pages/food-page/food-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    SearchBarComponent
+    SearchBarComponent,
+    TagsComponent,
+    FoodPageComponent
   ],
   imports: [
     BrowserModule,
--- client/src/app/services/food.service.ts
+++ client/src/app/services/food.service.ts
@@ -1,19 +1,35 @@
 import { Injectable } from '@angular/core';
-import { sample_foods } from 'src/data';
+import { sample_foods, sample_tags } from 'src/data';
 import { Food } from '../shared/models/food';
+import { Tag } from '../shared/models/tag';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FoodService {
-
-  constructor() { }
+  constructor() {}
 
   getAll(): Food[] {
     return sample_foods;
   }
 
-  getAllFoodsBySearchTerm(searhTerm:string) {
-    return this.getAll().filter(f => f.name.toLowerCase().includes(searhTerm.toLowerCase()));
+  getAllFoodsBySearchTerm(searhTerm: string) {
+    return this.getAll().filter((f) =>
+      f.name.toLowerCase().includes(searhTerm.toLowerCase())
+    );
+  }
+
+  getAllTags(): Tag[] {
+    return sample_tags;
+  }
+
+  getAllFoodsByTag(tag: string): Food[] {
+    return tag === 'All'
+      ? this.getAll()
+      : this.getAll().filter((f) => f.tags?.includes(tag));
+  }
+
+  getFoodById(foodId:string):Food {
+    return this.getAll().find(f => f.id === foodId) ?? new Food();
   }
 }
