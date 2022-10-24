5fb534c56900898e247e4abd6f2c1e7ba137429a
Frank Claveria <frankrobertclaveria@accenture.com>
2022-10-25 02:05:11 +0800

    add cart page and add not found logic

--- client/src/app/components/pages/cart-page/cart-page.component.ts
+++ client/src/app/components/pages/cart-page/cart-page.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { CartService } from 'src/app/services/cart.service';
+import { CartItem } from 'src/app/shared/models/cart-item.model';
+import { Cart } from 'src/app/shared/models/cart.model';
 
 @Component({
   selector: 'app-cart-page',
   templateUrl: './cart-page.component.html',
-  styleUrls: ['./cart-page.component.css']
+  styleUrls: ['./cart-page.component.css'],
 })
 export class CartPageComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  cart!: Cart;
+  constructor(private cartService: CartService) {
+    this.cartService.getCartObservable().subscribe((c) => {
+      this.cart = c;
+    });
   }
 
+  ngOnInit(): void {}
+
+  removeFromCart(cartItem: CartItem) {
+    this.cartService.removeFromCart(cartItem.food.id);
+  }
+
+  changeQuantity(cartItem: CartItem, quantityInString: string) {
+    const quantity = parseInt(quantityInString);
+
+    this.cartService.changeQuantity(cartItem.food.id, quantity);
+  }
 }
